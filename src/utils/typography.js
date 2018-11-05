b9e73f294aba203e199dd291707db1fc54a3fd4b
Cristiano Betta <cristiano@betta.io>
2018-11-05 23:39:30 +0000

    Simplify fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -7,12 +7,12 @@ export default new Typography({
   headerColor: '#151E25',
   baseLineHeight: 1.75,
   headerFontFamily: ['Lato', 'sans-serif'],
-  bodyWeight: 400,
+  bodyWeight: 300,
   headerWeight: 400,
   googleFonts: [
     {
       name: 'Lato',
-      styles: ['300', '400', '700']
+      styles: ['300']
     }
   ],
   overrideThemeStyles:  ({ rhythm }, options) => ({
@@ -26,6 +26,9 @@ export default new Typography({
       marginTop: rhythm(2),
       marginBottom: rhythm(1),
       fontWeight: 300
+    },
+    'h3': {
+      fontWeight: 400
     }
   })
 })
