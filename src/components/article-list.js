fd45838c4dde54a3c4026752bfba3e938e4ca616
Cristiano Betta <cristiano@betta.io>
2018-11-11 14:02:19 +0000

    Update how we render page components

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,46 +1,12 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
 
 import ArticleListItem from "./article-list-item"
 import { style } from "./article-list.module.scss"
 
-const render = (data) => (
+export default ({ data }) => (
   <div className={ style }>
     {data.allMarkdownRemark.edges.map(({ node }) => (
       <ArticleListItem {...node} key={node.id} />
     ))}
   </div>
 )
-
-export default () => (
-  <StaticQuery
-    query={query}
-    render={render}
-  />
-)
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      },
-    },
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "MMMM D, YYYY")
-            icon
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,33 @@
 import React from "react"
+import { graphql } from 'gatsby'
+
 import Layout from "../components/layout"
 import ArticleList from "../components/article-list"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <ArticleList />
+    <ArticleList data={data} />
   </Layout>
 )
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM D, YYYY")
+            icon
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
