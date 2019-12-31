12b99db685efe50482be15cb7ab1907b900e3881
Cristiano Betta <cbetta@box.com>
2019-12-31 18:11:51 +0000

    Add border to talk title:

--- src/components/talks/List.js
+++ src/components/talks/List.js
@@ -1,20 +1,20 @@
 import React from "react"
 
 import ListItem from "./ListItem"
-import { style } from "./List.module.scss"
+import style from "./List.module.scss"
 
 export default ({ talks, title }) => (
-  <div className={ style }>
+  <div className={ style.list }>
     <header><span>{ title || "Talks" }</span></header>
 
-  <div>
-    <p>
-      I've had the honor to have spoken at the events below. I love to
-      talk about Developer Experience and Developer Relations, and I'm
-      fascinated with the boundary where technology and culture meet. 
-    </p>
+    <div className={style.description}>
+      <p>
+        I've had the honor to have spoken at the events below. I love to
+        talk about Developer Experience and Developer Relations, and I'm
+        fascinated with the boundary where technology and culture meet. 
+      </p>
+    </div>
 
-  </div>
     { talks.map(talk => (
       <ListItem {...talk} key={talk.id} />
     ))}
