9711d627af1f2fd379372f9458382117b263acc5
Cristiano Betta <cbetta@box.com>
2019-12-31 18:08:32 +0000

    Fix label padding

--- src/components/talks/List.js
+++ src/components/talks/List.js
@@ -7,6 +7,14 @@ export default ({ talks, title }) => (
   <div className={ style }>
     <header><span>{ title || "Talks" }</span></header>
 
+  <div>
+    <p>
+      I've had the honor to have spoken at the events below. I love to
+      talk about Developer Experience and Developer Relations, and I'm
+      fascinated with the boundary where technology and culture meet. 
+    </p>
+
+  </div>
     { talks.map(talk => (
       <ListItem {...talk} key={talk.id} />
     ))}
