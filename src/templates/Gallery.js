e4c9e2d6dd533637bd5325624e84242fddea4793
Cristiano Betta <cbetta@box.com>
2020-01-01 13:44:45 +0000

    Use slug as social image url

--- src/templates/Gallery.js
+++ src/templates/Gallery.js
@@ -6,27 +6,27 @@ import { Helmet } from "react-helmet"
 import Layout from "../layouts/Layout"
 import Gallery from '../components/galleries/Gallery'
 
-export default ({ data: { site, gallery }, path }) => (
+export default ({ data: { site, gallery } }) => (
   <Layout>
     <Helmet>
       <title>{gallery.frontmatter.title}</title>
       
       <meta name="description" content={ gallery.excerpt } />
       <meta name="description" content={ gallery.frontmatter.tags } />
-      <meta name="image" content={`${site.siteMetadata.siteUrl}${path}twitter-card.jpg`} />
+      <meta name="image" content={`${site.siteMetadata.siteUrl}${gallery.fields.slug}twitter-card.jpg`} />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={`${site.siteMetadata.siteUrl}${path}twitter-card.jpg`} />
+      <meta name="twitter:image" content={`${site.siteMetadata.siteUrl}${gallery.fields.slug}twitter-card.jpg`} />
       <meta name="twitter:title" content={gallery.frontmatter.title} />
       <meta name="twitter:description" content={gallery.excerpt}  />
       <meta name="twitter:site" content='@cbetta'  /> 
 
-      <meta property="og:url" content={`${site.siteMetadata.siteUrl}${path}`} />
+      <meta property="og:url" content={`${site.siteMetadata.siteUrl}${gallery.fields.slug}`} />
       <meta property="og:title" content={gallery.frontmatter.title} />
       <meta property="og:description" content={gallery.excerpt} />
-      <meta property="og:image" content={`${site.siteMetadata.siteUrl}${path}twitter-card.jpg`} />
-      <meta property="og:image:url" content={`${site.siteMetadata.siteUrl}${path}twitter-card.jpg`} />
-      <meta property="og:image:secure_url" content={`${site.siteMetadata.siteUrl}${path}twitter-card.jpg`} />
+      <meta property="og:image" content={`${site.siteMetadata.siteUrl}${gallery.fields.slug}twitter-card.jpg`} />
+      <meta property="og:image:url" content={`${site.siteMetadata.siteUrl}${gallery.fields.slug}twitter-card.jpg`} />
+      <meta property="og:image:secure_url" content={`${site.siteMetadata.siteUrl}${gallery.fields.slug}twitter-card.jpg`} />
       <meta property="og:image:type" content="image/jpg" />
       <meta property="og:image:width" content="600" />
       <meta property="og:image:height" content="314" />
