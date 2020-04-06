e29eda77ce71d0a50d57b844ed74ddd82b2e972d
Cristiano Betta <cbetta@box.com>
2020-04-06 14:38:53 +0100

    Remove social card support

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,29 +73,7 @@ const Markdown = [
             noInlineHighlight: false,
           },
         },
-        "gatsby-remark-copy-linked-files",
-        {
-          resolve: `gatsby-remark-social-cards`,
-          options: {
-            title: {
-              style: "bold",
-              color: "black",
-              size: 48
-            },
-            meta: {
-              parts: [
-                "Cristiano Betta",
-                " - ",
-                { field: "date", format: "mmmm dS, yyyy"},
-              ],
-              color: 'black',
-              size: 32
-            },
-            xMargin: 30,
-            yMargin: 30,
-            background: "#fefefe"
-          }
-        }
+        "gatsby-remark-copy-linked-files"
       ],
     },
   },
